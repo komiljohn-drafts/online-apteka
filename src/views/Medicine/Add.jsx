8619ebe667a353ace0588dbd467d84e99e53d959
komiljon_yormurodov <komiljonyormurodov@gmail.com>
2022-06-20 21:34:20 +0500

    fix building issue

--- src/views/Medicine/Add.jsx
+++ src/views/Medicine/Add.jsx
@@ -1,12 +1,10 @@
-import Button from "../../components/Button"
-import Header from "../../components/Header"
-import { CancelIcon, SaveIcon } from "../../assets/icons/svgs"
-import Card from "../../components/Card"
-import MedicineForm from "../../forms/Medicine"
-import { useForm } from "react-hook-form"
-import { useNavigate } from "react-router-dom"
-import axios from "axios"
-import { useEffect } from "react"
+import Button from '../../components/Button'
+import Header from '../../components/Header'
+import { CancelIcon, SaveIcon } from '../../assets/icons/svgs'
+import Card from '../../components/Card'
+import MedicineForm from '../../forms/Medicine'
+import { useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
 
 const MedicineAdd = () => {
   const {
@@ -18,9 +16,7 @@ const MedicineAdd = () => {
 
   const navigate = useNavigate()
 
-  useEffect(() => axios.get("http://localhost:3001/").then((res) => console.log("response - ", res)), [])
-
-  const onSubmit = (data) => console.log("medicine form data - ", data)
+  const onSubmit = (data) => console.log('medicine form data - ', data)
 
   return (
     <div>
@@ -29,18 +25,18 @@ const MedicineAdd = () => {
           title="Dori qo'shish"
           backLink={-1}
           rightElement={
-            <div className="flex gap-3">
-              <Button leftIcon={<CancelIcon />} shape="secondary" onClick={() => navigate(-1)}>
+            <div className='flex gap-3'>
+              <Button leftIcon={<CancelIcon />} shape='secondary' onClick={() => navigate(-1)}>
                 Bekor qilish
               </Button>
-              <Button leftIcon={<SaveIcon />} type="submit">
+              <Button leftIcon={<SaveIcon />} type='submit'>
                 Saqlash
               </Button>
             </div>
           }
         />
-        <div className="p-4">
-          <Card title="Dori qo'shish" width="70%">
+        <div className='p-4'>
+          <Card title="Dori qo'shish" width='70%'>
             <MedicineForm errors={errors} register={register} control={control} />
           </Card>
         </div>
